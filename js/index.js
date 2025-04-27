14b7f749aaf4df5631bbd5c0c2cac668fd2b6c4f
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-04-27 15:43:32 +0300

    feat: integrate hamburger menu and schema.org markup

--- js/index.js
+++ js/index.js
@@ -1,20 +1,35 @@
-const TO_TOP = document.querySelector('.pageUp');
-const HEADING_ARROW = document.querySelector('.heading__arrow');
-const FOOTER_YEAR = document.querySelector('#year');
+document.addEventListener('DOMContentLoaded', () => {
+  const TO_TOP = document.querySelector('.pageUp');
+  const HEADING_ARROW = document.querySelector('.heading__arrow');
+  const FOOTER_YEAR = document.querySelector('#year');
+  const HAMBURGER = document.querySelector('.header__wrapper-hamburger');
+  const NAV = document.querySelector('.header__wrapper-item-nav');
 
-HEADING_ARROW.onclick = function () {
-  window.scrollTo(0, window.innerHeight);
-};
+  HEADING_ARROW.onclick = function () {
+    window.scrollTo(0, window.innerHeight);
+  };
 
-window.addEventListener('scroll', function () {
-  if (window.scrollY > 1000) {
-    TO_TOP.classList.remove('hidden');
-  } else {
-    TO_TOP.classList.add('hidden');
-  }
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 1000) {
+      TO_TOP.classList.remove('hidden');
+    } else {
+      TO_TOP.classList.add('hidden');
+    }
+  });
+  TO_TOP.onclick = function () {
+    window.scrollTo(0, 0);
+  };
+
+  FOOTER_YEAR.textContent = String(new Date().getFullYear());
+
+  HAMBURGER.onclick = function () {
+    NAV.classList.toggle('active');
+    HAMBURGER.classList.toggle('active');
+    document.body.classList.toggle('fixed');
+  };
+  NAV.onclick = function () {
+    NAV.classList.toggle('active');
+    HAMBURGER.classList.toggle('active');
+    document.body.classList.toggle('fixed');
+  };
 });
-TO_TOP.onclick = function () {
-  window.scrollTo(0, 0);
-};
-
-FOOTER_YEAR.textContent = String(new Date().getFullYear());
